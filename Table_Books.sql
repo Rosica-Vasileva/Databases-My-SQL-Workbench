4119d1a1f5a4b6b979d9c0270dc7a826c389336a
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-02-14 13:35:53 +0200

    Add files via upload

---
+++ Table_Books.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Books (BookID,Title,Author,PublicationYear,AvaliableCopies)
+VALUES 
+(1,'Harry Pother','J.K.Rowling',1997,10),
+(2,'To Kill a Mockingbirds','H.Lee',1960,9),
+(3,'1984','G.Orwell',1949,5),
+(4,'The Great Gatsby','F.S.Fitzgerald',1925,3),
+(5,'The Cather in the Rye','J.S.D',1951,3);
